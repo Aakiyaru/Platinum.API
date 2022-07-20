5d9d40286d3f8642e636035a54185c0903a302a4
Denis Bondarenko <JellyBall@mail.ru>
2022-07-20 22:55:18 +0700

    Создан абстрактный класс запросов к БД

---
+++ Knowtes.WebAPI/Commands/Command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+namespace Knowtes.WebAPI.Commands
+{
+    public abstract class Command
+    {
+        private static string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"AppData\DataBase", "DataBase.db");
+        private static SQLiteConnection connection = new SQLiteConnection($@"Data Source={filePath}; Version=3");
+        private SQLiteCommand command;
+
+        protected void Create(string commandText)
+        {
+            command = connection.CreateCommand();
+            connection.Open();
+            command.CommandText = commandText;
+        }
+
+        protected void Dispose()
+        {
+            command.Dispose();
+            connection.Close();
+            connection.Dispose();
+        }
+    }
+}
