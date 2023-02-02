1e58b38bb28d20bffc264a1e5e64992523f5d860
Denis Bondarenko <JellyBall@mail.ru>
2023-02-02 08:26:22 +0700

    запросы на получение данных о достижениях

---
+++ Knowtes.WebAPI/Querries/Achivements/GetAchivementQuerry.cs
@@ -0,0 +1,17 @@
+﻿using Knowtes.WebAPI.Querries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platinum.WebAPI.Querries.Achivements
+{
+    public class GetAchivementQuerry : Querry
+    {
+        public static void Set(int id, int gameId)
+        {
+            querryText = $"SELECT Id, Name, Description, GameId, Icon FROM Achivements WHERE Id = {id} AND gameId = {gameId}";
+        }
+    }
+}
