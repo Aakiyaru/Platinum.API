c832c2c96c780d174880631290139a1bb1bd7be2
Denis Bondarenko <JellyBall@mail.ru>
2023-04-20 11:45:13 +0700

    Добавлен поиск игры по названию

---
+++ Knowtes.WebAPI/Querries/Games/GetSearchedGamesQuerry.cs
@@ -0,0 +1,17 @@
+﻿using Knowtes.WebAPI.Querries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platinum.WebAPI.Querries.Games
+{
+    public class GetSearchedGamesQuerry : Querry
+    {
+        public static void Set(string searchString)
+        {
+            querryText = $"SELECT Name, Cover, Id FROM Games WHERE name LIKE \"%{searchString}%\";";
+        }
+    }
+}
